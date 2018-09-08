c81691ba0aba7bf10ee4b79642193f10342374f2
Drake Morrison <drakethedragonprogrammer@gmail.com>
2018-09-08 13:11:46 -0500

    More Git Redo

---
+++ CorporateClass/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorporateClass
+{
+    class Employee
+    {
+        public string EmployeeName { get; set; }
+        public DateTime StartDate;
+        public string JobTitle;
+
+        public Employee(string name, string title)
+        {
+            changeName(name);
+            startJob(DateTime.Now, title);
+        }
+
+        public void changeName(string name)
+        {
+            EmployeeName = name;
+        }
+
+        public void startJob(DateTime startDate, string jobTitle)
+        {
+            StartDate = startDate;
+            JobTitle = jobTitle;
+        }
+    }
+}
---
+++ CorporateClass/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CorporateClass
+{
+    class Program
+    {
+        public class Company
+        {
+            /*
+                Some readonly properties
+            */
+            public string Name { get; }
+            public DateTime CreatedOn { get; }
+
+            // Create a property for holding a list of current employees
+            public List<Employee> Employees { get; set; }
+
+            // Create a method that allows external code to add an employee
+            public void addEmployee(Employee empl)
+            {
+                Employees.Add(empl);
+            }
+
+            // Create a method that allows external code to remove an employee
+            public void removeEmployee(Employee empl)
+            {
+                Employees.Remove(empl);
+            }
+            /*
+                Create a constructor method that accepts two arguments:
+                    1. The name of the company
+                    2. The date it was created
+
+                The constructor will set the value of the public properties
+            */
+            public Company(string nameOfCompany, DateTime createdOn)
+            {
+                Name = nameOfCompany;
+                CreatedOn = createdOn;
+                Employees = new List<Employee>();
+            }
+
+            public void ListEmployees()
+            {
+                foreach(Employee dude in Employees)
+                {
+                    Console.WriteLine(dude.EmployeeName);
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Company NSS = new Company("NSS", DateTime.Now);
+            Employee steve = new Employee("steve", "Coach");
+            Employee john = new Employee("john", "CEO");
+            Employee nathan = new Employee("nathan", "teacher");
+
+            NSS.addEmployee(steve);
+            NSS.addEmployee(john);
+            NSS.addEmployee(nathan);
+            NSS.ListEmployees();
+
+            Console.ReadLine();
+        }
+    }
+}
