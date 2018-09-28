8911728247000e2f2e010be725289645b4c86dec
Drake Morrison <drakethedragonprogrammer@gmail.com>
2018-09-28 14:51:14 -0500

    IntToRoman function is working, started work on RomantoINt

--- RomanNumeralConverter.Test/UnitTest1.cs
+++ RomanNumeralConverter.Test/UnitTest1.cs
@@ -10,10 +10,27 @@ namespace RomanNumeralConverter.Test
         [Theory]
         [InlineData(1, "I")]
         [InlineData(5, "V")]
-        [InlineData(9, "IV")]
+        [InlineData(9, "IX")]
         [InlineData(12, "XII")]
         [InlineData(16, "XVI")]
         [InlineData(29, "XXIX")]
+        [InlineData(44, "XLIV")]
+        [InlineData(45, "XLV")]
+        [InlineData(68, "LXVIII")]
+        [InlineData(83, "LXXXIII")]
+        [InlineData(97, "XCVII")]
+        [InlineData(99, "XCIX")]
+        [InlineData(500, "D")]
+        [InlineData(501, "DI")]
+        [InlineData(649, "DCXLIX")]
+        [InlineData(798, "DCCXCVIII")]
+        [InlineData(891, "DCCCXCI")]
+        [InlineData(1000, "M")]
+        [InlineData(1004, "MIV")]
+        [InlineData(1006, "MVI")]
+        [InlineData(1023, "MXXIII")]
+        [InlineData(2014, "MMXIV")]
+        [InlineData(3999, "MMMCMXCIX")]
         public void convert_a_number_to_a_roman_numeral(int input, string expectedResult)
         {
             //Arrange       
@@ -21,7 +38,20 @@ namespace RomanNumeralConverter.Test
             //Act
             var result = converter.IntToRoman(input);
             //Assert
-            Assert.Equal(result, expectedResult);
+            Assert.Equal(expectedResult, result);
+        }
+        
+        [Theory]
+        [InlineData("XVI", 16)]
+        [InlineData("XXIX", 29)]
+        public void convert_a_roman_numeral_to_an_integer(string input, int expectedResult)
+        {
+            //Arrange
+            var converter = new RomanConverter();
+            //Act
+            var result = converter.RomanToInt(input);
+            //Assert
+            Assert.Equal(expectedResult, result);
         }
     }
 }
--- RomanNumeralConverter/Program.cs
+++ RomanNumeralConverter/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RomanNumeralConverter
 {
     public class Program
-    {       
+    {
         static void Main(string[] args)
         {
             Console.WriteLine("Give me a number, so I can make it a Roman Numeral");
@@ -13,40 +14,122 @@ namespace RomanNumeralConverter
 
             var converter = new RomanConverter();
 
-            var result = converter.IntToRoman(numberEntered);
+            var romanNumeralResult = converter.IntToRoman(numberEntered);
 
-            Console.WriteLine(result);            
-        }        
+            Console.WriteLine(romanNumeralResult);
+            Console.ReadLine();
+
+            Console.WriteLine("now give me a number made with roman numerals in all caps");
+
+            var input = Console.ReadLine();
+
+            var integerResult = converter.RomanToInt(input);
+
+            Console.WriteLine(integerResult);
+            Console.ReadLine();
+        }
+    }
+
+    public class RomanNumeral
+    {
+        public int Value { get; set; }
+        public string Numeral { get; set; }
+
+        public RomanNumeral(string numeral, int value)
+        {
+            Value = value;
+            Numeral = numeral;
+        }
+    }
+
+    public class RomanCharacter
+    {
+        public int Value { get; set; }
+        public char Numeral { get; set; }
+
+        public RomanCharacter(char numeral, int value)
+        {
+            Value = value;
+            Numeral = numeral;
+        }
     }
 
     public class RomanConverter
     {
 
-        public Dictionary<string, int> RomanMap = new Dictionary<string, int>()
+        public List<RomanCharacter> RomanMapAscending = new List<RomanCharacter>()
         {
-            { "I", 1},
-                { "IV", 4 },
-                { "V", 5 },
-                { "IX", 9 },
-                { "X", 10 },
-                { "XL", 40 },
-                { "L", 50 },
-                { "XC", 90 },
-                { "C", 100 },
-                { "CD", 400 },
-                { "D", 500 },
-                { "CM", 900 },
-                { "M", 1000 }
+            new RomanCharacter('I', 1),
+            new RomanCharacter('IV', 4 ),
+            new RomanCharacter( 'V', 5 ),
+            new RomanCharacter( 'IX', 9 ),
+            new RomanCharacter( 'X', 10 ),
+            new RomanCharacter( 'XL', 40 ),
+            new RomanCharacter( 'L', 50 ),
+            new RomanCharacter( 'XC', 90 ),
+            new RomanCharacter( 'C', 100 ),
+            new RomanCharacter( 'CD', 400 ),
+            new RomanCharacter( 'D', 500 ),
+            new RomanCharacter( 'CM', 900 ),
+            new RomanCharacter( 'M', 1000 )
+        };
+
+        public List<RomanNumeral> RomanMapDescending = new List<RomanNumeral>()
+        {
+            new RomanNumeral( "M", 1000 ),
+            new RomanNumeral( "CM", 900 ),
+            new RomanNumeral( "D", 500 ),
+            new RomanNumeral( "CD", 400 ),
+            new RomanNumeral( "C", 100 ),
+            new RomanNumeral( "XC", 90 ),
+            new RomanNumeral( "L", 50 ),
+            new RomanNumeral( "XL", 40 ),
+            new RomanNumeral( "X", 10 ),
+            new RomanNumeral( "IX", 9 ),
+            new RomanNumeral( "V", 5 ),
+            new RomanNumeral("IV", 4 ),
+            new RomanNumeral("I", 1)
         };
 
         public string IntToRoman(int input)
         {
-            throw new NotImplementedException();
+            var numberToBuild = input;
+            var output = "";
+            int howManyOfNumeral;
+            int remainder;
+
+            for (var i = 0; i < RomanMapDescending.Count; i++)
+            {
+                howManyOfNumeral = Math.Abs(numberToBuild / RomanMapDescending[i].Value);
+                remainder = numberToBuild % RomanMapDescending[i].Value;
+
+                for (int y = 0; y < howManyOfNumeral; y++)
+                {
+                    output += RomanMapDescending[i].Numeral;
+                }
+
+                numberToBuild = remainder;
+            }
+
+            return output;
         }
 
         public int RomanToInt(string input)
         {
-            throw new NotImplementedException();
+            var romanNumberText = input;
+            var romanNumbers = romanNumberText.ToCharArray().ToList();
+            var output = 0;
+
+            for (int i = 0; i < RomanMapAscending.Count; i++)
+            {
+                while (romanNumbers[romanNumbers.Count - 1] == RomanMapAscending[i].Numeral)
+                {
+                    output += RomanMapAscending[i].Value;
+                    romanNumbers.RemoveAt(romanNumbers.Count - 1);
+                }
+            }
+
+            return output;
         }
     }
 }
